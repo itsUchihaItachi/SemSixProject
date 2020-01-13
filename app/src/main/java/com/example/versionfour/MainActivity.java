7fbe7188dfb0b8c17aea6833f7616c123471f42e
Malfoy <rockstarodmrel@gmail.com>
2020-01-13 23:17:00 +0530

    Much better Login Screen

--- app/src/main/java/com/example/versionfour/MainActivity.java
+++ app/src/main/java/com/example/versionfour/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login_layout);
         myDB=new DatabaseHelper(this);
         username = findViewById(R.id.erpEditText);
         password = findViewById(R.id.passwordEditText);
