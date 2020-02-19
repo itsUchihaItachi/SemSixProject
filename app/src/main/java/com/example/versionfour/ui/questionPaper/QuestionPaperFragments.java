5418e4549e25cb5aab783ec6d0b1807e9aa12ae8
itsUchihaItachi <sanketkamble855@gmail.com>
2020-02-19 19:09:59 +0530

    Check question paper frag

---
+++ app/src/main/java/com/example/versionfour/ui/questionPaper/QuestionPaperFragments.java
@@ -0,0 +1,40 @@
+package com.example.versionfour.ui.questionPaper;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.viewpager.widget.ViewPager;
+
+import com.example.versionfour.R;
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.tabs.TabLayout;
+
+public class QuestionPaperFragments extends  Fragment {
+
+    private TabLayout tabLayout;
+    private AppBarLayout appBarLayout;
+    private ViewPager viewPager;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_forums, container, false);
+        tabLayout =  root.findViewById(R.id.question_tabs);
+        // check fragment_question_paper
+        appBarLayout =root.findViewById(R.id.appBarQP);
+        viewPager = root.findViewById(R.id.question_paper_viewpager);
+
+        QuestionViewPagerAdapter adapter = new QuestionViewPagerAdapter(getChildFragmentManager());
+        adapter.addFragment(new FragmentQuiz(),"Quiz");
+        adapter.addFragment(new FragmentExplore(),"Explore");
+        adapter.addFragment(new FragmentStore(),"Store");
+
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+        return root;
+    }
+}
