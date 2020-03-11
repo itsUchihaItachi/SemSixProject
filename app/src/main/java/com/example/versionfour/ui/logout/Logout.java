5a384b9bf794e5cb43061ae1be12ddcae2c204c5
itsUchihaItachi <sanketkamble855@gmail.com>
2020-03-11 19:57:42 +0530

    question paper frag img changes

---
+++ app/src/main/java/com/example/versionfour/ui/logout/Logout.java
@@ -0,0 +1,21 @@
+package com.example.versionfour.ui.logout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.versionfour.MainActivity;
+
+public class Logout extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+//        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+//        startActivity(intent);
+    }
+}
